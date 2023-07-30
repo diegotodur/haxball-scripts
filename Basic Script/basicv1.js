f6dc8b3ae1abd4c074cdd1e2f1d068c10db761c1
DiegoTorres <miscelanneo@gmail.com>
2023-07-30 18:49:10 -0400

    [UPDATE] fix err

--- Basic Script/basicv1.js
+++ Basic Script/basicv1.js
@@ -1,24 +1,3 @@
-//Funcion para mandar anuncios
-const announce = (msg, targetId, color = config.getColor('anuncio'), style = "normal", sound = 0) => {
-	room.sendAnnouncement(msg, targetId, color, style, sound);
-};
-
-//Actualizar admins
-const updateAdmins = () => {
-	var players = room.getPlayerList();
-	if (players.length == 0) return;
-	if (players.find((player) => player.admin) != null) return;
-	room.setPlayerAdmin(players[0].id, true);
-}
-
-//Si la sala esta vacia, se pausa el juego.
-const pausarSalaVacia = () => {
-	var players = room.getPlayerList();
- 	if ( players.length == 0 ){
-		room.stopGame()
-	}
-}
-
 class Config {
 	constructor() {
 	  this.roomName = 'Haxball Scripts';
@@ -38,30 +17,46 @@ class Config {
 		anuncioBienvenida: ', bienvenido a la sala!',
 	  };
 	}
-  
-	setColor(key, color) {
-	  this.colores[key] = color;
-	}
-  
+
 	getColor(key) {
 	  return this.colores[key];
 	}
-  
-	setMessage(key, message) {
-	  this.mensajes[key] = message;
-	}
-  
 	getMessage(key) {
 	  return this.mensajes[key];
 	}
 }
 const config = new Config();
+const { getColor, getMessage } = config;
+
+
+//Funcion para mandar anuncios
+const announce = (msg, targetId, color =  config.getColor('anuncio'), style = "normal", sound = 0) => {
+	room.sendAnnouncement(msg, targetId, color, style, sound);
+};
+
+//Actualizar admins
+const updateAdmins = () => {
+	var players = room.getPlayerList();
+	if (players.length == 0) return;
+	if (players.find((player) => player.admin) != null) return;
+	room.setPlayerAdmin(players[0].id, true);
+}
+
+//Si la sala esta vacia, se pausa el juego.
+const pausarSalaVacia = () => {
+	var players = room.getPlayerList();
+ 	if ( players.length == 0 ){
+		room.stopGame()
+	}
+}
+
 
 const room = HBInit({
 	roomName: config.roomName,
 	maxPlayers: config.maxPlayers,
 	noPlayer: config.noPlayer,
-	public: config.public
+	public: config.public,
+	password: config.password
 });
 
 
@@ -72,7 +67,7 @@ room.onPlayerJoin = function (player) {
 
 	const auth = player.auth;
 	const nombre = player.name
-	const msj = `Hola ${player.name}, ${getMessage('anuncioBienvenida')}`;
+	const msj = `Hola ${player.name}, ${config.getMessage('anuncioBienvenida')}`;
 	announce(msj, player.id);
 
 	const playerObject = {
